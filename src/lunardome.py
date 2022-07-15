6e93abe0198805b81b88c56659eb85980483df9c
idunmore <62761638+idunmore@users.noreply.github.com>
2022-07-15 14:03:48 -0400

    Abstracts text colorization and adds shortcuts

--- src/lunardome.py
+++ src/lunardome.py
@@ -8,8 +8,10 @@
 
 from enum import Enum
 import random
+from sqlite3 import IntegrityError
 import sys
 import os
+from tkinter import Y
 from sty import fg, ef
 
 # Constants
@@ -18,18 +20,29 @@ MAX_INTEGRITY = 100
 # Multiplier for each difficulty "level"; higher numbers = harder.
 DIFFICULTY_MULTIPLIER = [1, 1.25, 1.5, 1.75, 2.00]
 
-# Text color/effect aliases (from "sty" module values).  C_ prefix = "Color".
+# Text color/effect aliases (from "sty" module values).  C class = "Color".
 # Abbreviations are used to reduce line-length in text output functions.  
-C_SOUP = fg.yellow      # Soup
-C_EMPH = fg.white       # Emphasis
-C_OXY = fg.li_blue      # Oxygen
-C_SCULPT = fg.magenta   # Lunar Scupltures
-C_INTEG = fg.cyan       # Dome Integrity
-C_GOOD = fg.green       # Good/Boon
-C_BAD = fg.red          # Bad/Calamity/Warning
-C_EASY = fg.green       # Easy
-C_HARD = fg.red         # Hard
-C_OFF = fg.rs + ef.rs   # Default State Color (Color "OFF", Effects "OFF")
+class C():
+    Soup = fg.yellow        # Soup
+    Emph = fg.white         # Emphasis
+    Oxy = fg.li_blue        # Oxygen
+    Sculpt = fg.magenta     # Lunar Sculptures
+    Integ = fg.cyan         # Integrity
+    Credit = fg.li_green    # Credit
+    Good = fg.green         # Good/Boon
+    Bad = fg.red            # Bad/Calamity
+    Easy = fg.green         # Easy
+    Hard = fg.red           # Hard
+    Off = fg.rs + ef.rs     # Default Color/Effect (Color="OFF", Effects="OFF")
+
+# Text with coloring/effects applied (shortcuts for string assembly)
+class CText():
+    Soup = f"{C.Soup}Soup{C.Off}"
+    Oxygen = f"{C.Oxy}Oxygen{C.Off}"
+    Integrity = f"{C.Integ}Integrity{C.Off}"
+    Sculptures = f"{C.Sculpt}Lunar Sculptures{C.Off}"
+    Credits = f"{C.Credit}Credits{C.Off}"
+    Colonists = f"{C.Emph}Colonists{C.Off}"
 
 class EventType(Enum): 
     BOON = 0
@@ -113,38 +126,45 @@ class DomeState():
 
     def display(self):
         clear_screen()
-        print(f"There are {ef.bold}{self.colonists:,d}{fg.rs}{ef.rs} colonists "
-            f"living in the dome, in year {fg.green}{self.year:,d}{fg.rs}.")      
-        print(f"Available credits: {fg.green}{self.credits:,d}{fg.rs}.")
-        print(f"Dome integrity is at {self.integrity:d}%; annual maintenance "
-            f"cost is {fg.green}{self.maintenance_cost:n}{fg.rs} credits.")            
-        print(f"Soup stocks stand at {self.soup:,d} units.")        
-        print(f"Each colonist requires {self.soup_required_per_colonist:n} "
-            f"units of Soup per year, at {self.soup_cost:n} credits per unit.")
+        print(f"There are {C.Emph}{self.colonists:,d}{C.Off} colonists "
+            f"living in the dome, in year {C.Emph}{self.year:,d}{C.Off}.")      
+        print(f"Available credits: {C.Credit}{self.credits:,d}{C.Off}.")
+        print(f"Dome integrity is at {C.Integ}{self.integrity:d}%{C.Off}; "
+            f"annual maintenance is {C.Good}{self.maintenance_cost:n}{C.Off} "
+            f"credits.")            
+        print(f"{CText.Soup} stocks stand at {C.Soup}{self.soup:,d}{C.Off} "
+            f"units.")        
+        print(f"Each colonist requires {C.Soup}"
+            f"{self.soup_required_per_colonist:n}{C.Off} units of {CText.Soup} "
+            f"per year, at {C.Credit}{self.soup_cost:n}{C.Off} credits "
+            f"per unit.")
         if self.is_medium_or_lower_difficulty:
             soup_lasts = int((self.soup
                 / (self.colonists * self.soup_required_per_colonist))) 
-            print(f"Current Soup stocks will last about {soup_lasts:n} years "
-                "at present population.")
+            print(f"Current {CText.Soup} stocks will last about "
+              f"{C.Soup}{soup_lasts:n}{C.Off} years at present population.")
             soup_total_cost = (self.soup_cost
                 * self.soup_required_per_colonist * self.colonists)         
-            print("A one year supply of Soup for all colonists costs "
-                f"{soup_total_cost:,d} credits.")            
-        print(f"Oxygen tanks currently hold {self.oxygen:,d} units of Oxygen.")
-        print(f"Each colonist requires {self.oxygen_required_per_colonist:n} "
-            f"units of Oxygen per year, at {self.oxygen_cost:n} "
-            "credits per unit.")
+            print(f"A one year supply of {CText.Soup} for all colonists "
+                f"costs {C.Credit}{soup_total_cost:,d}{C.Off} credits.")            
+        print(f"{CText.Oxygen} tanks currently hold {C.Oxy}{self.oxygen:,d}"
+            f"{C.Off} units of {CText.Oxygen}.")
+        print(f"Each colonist requires {C.Oxy}"
+            f"{self.oxygen_required_per_colonist:n}{C.Off} units of "
+            f"{CText.Oxygen} per year, at {C.Credit}{self.oxygen_cost:n}"
+            f"{C.Off} credits per unit.")
         if self.is_medium_or_lower_difficulty:
             oxygen_lasts = int((self.oxygen
                 / (self.oxygen_required_per_colonist * self.colonists)))
-            print(f"Current Oxygen stores will last about {oxygen_lasts:n} "
-                "years at present population.")
+            print(f"Current {CText.Oxygen} stores will last about {C.Oxy}"
+                f"{oxygen_lasts:n}{C.Off} years at present population.")
             oxygen_total_cost = (self.oxygen_cost
                 * self.oxygen_required_per_colonist * self.colonists)
-            print("A one year supply of Oxygen for all colonists costs "
-                f"{oxygen_total_cost:,d} credits.")                  
-        print(f"Lunar Scuplutures cost {self.sculpture_cost:n} units of Oxygen "
-            f"to make. They sell for {self.sculpture_value:n} credits.")
+            print(f"A one year supply of {CText.Oxygen} for all colonists "
+                f"costs {C.Credit}{oxygen_total_cost:,d}{C.Off} credits.")                  
+        print(f"{CText.Sculptures} cost {C.Oxy}{self.sculpture_cost:n}{C.Off} "
+            f"units of {CText.Oxygen} to make. They sell for {C.Credit}"
+            f"{self.sculpture_value:n}{C.Off} credits.")
         print("")
 
     def end_turn(self):
@@ -172,13 +192,12 @@ class DomeState():
             print(f"{fg.green}No dome maintenance required this year.{fg.rs}")
         elif self.credits >= self.maintenance_cost:
             print(f"Dome {fg.green}repaired{fg.rs} for "
-                f"{fg.white}{self.maintenance_cost:,d}{fg.rs} credits; now at "
-                f"{fg.green}100%{fg.li_cyan} Integrity{fg.rs}.")
+                f"{fg.green}{self.maintenance_cost:,d}{fg.rs} credits; now at "
+                f"{C.Integ}100% {CText.Integrity}.")
             self.credits -= self.maintenance_cost
-            self.integrity = 100
-            
+            self.integrity = 100            
         else:
-            print(f"{fg.red}Insufficient credits to repair dome.{fg.rs}")    
+            print(f"{fg.red}Insufficient credits to repair dome!{fg.rs}")    
 
 class Event():
     def __init__(self, event_type, commodity, minimum, maximum, message):
@@ -213,8 +232,8 @@ class Event():
 
     def __display(self):
         # Display BOONs as "Good", CALAMITIES as "Bad"       
-        color = C_GOOD if self.event_type == EventType.BOON else C_BAD
-        print(f"{color}{self.message.format(units=abs(self.amount))}{C_OFF}")
+        color = C.Good if self.event_type == EventType.BOON else C.Bad
+        print(f"{color}{self.message.format(units=abs(self.amount))}{C.Off}")
               
 
 # Calamities and Boons
@@ -281,12 +300,12 @@ def buy_commodity(commodity: Commodity, dome_state: DomeState):
     match commodity:
         case Commodity.OXYGEN:
             can_afford = int(dome_state.credits/dome_state.oxygen_cost)
-            commodity_name = f"{C_OXY}Oxygen{C_OFF}"
+            commodity_name = CText.Oxygen
             cost_per_unit = dome_state.oxygen_cost              
         case Commodity.SOUP:
             can_afford = int(dome_state.credits/dome_state.soup_cost)
-            commodity_name = f"{C_SOUP}Soup{C_OFF}"
-            cost_per_unit = dome_state.soup_cost              
+            commodity_name = CText.Soup
+            cost_per_unit = dome_state.soup_cost         
         case Commodity.INTEGRITY:
             sys.exit("Cannot buy Integrity; Error in main gaim loop Logic.") 
     
@@ -294,8 +313,8 @@ def buy_commodity(commodity: Commodity, dome_state: DomeState):
     units = get_amount(prompt, 0, can_afford)
     
     # Apply results of Purchase
-    print(f"You bought {C_EMPH}{units:n}{C_OFF} units of {commodity_name} for "
-        f"a total of {C_EMPH}{units * cost_per_unit:n}{C_OFF} credits.")
+    print(f"You bought {C.Emph}{units:n}{C.Off} units of {commodity_name} for "
+        f"a total of {C.Emph}{units * cost_per_unit:n}{C.Off} credits.")
 
     # Update purchased commodity value
     if commodity == Commodity.OXYGEN:
@@ -308,15 +327,15 @@ def buy_commodity(commodity: Commodity, dome_state: DomeState):
     print(f"You have {dome_state.credits:n} credits remaining.")
 
 def make_scupltures(dome_state:DomeState):
-    prompt = (f"How many {C_SCULPT}Lunar Sculptures{C_OFF} do you want to make "
+    prompt = (f"How many {C.Sculpt}Lunar Sculptures{C.Off} do you want to make "
         "and sell?")
     can_afford = int(dome_state.oxygen / dome_state.sculpture_cost)
     sculptures = get_amount(prompt, 0, can_afford)
     sculpture_profit = dome_state.sculpture_value * sculptures
     sculpture_oxygen_usage = sculptures * dome_state.sculpture_cost
-    print(f"You made {C_EMPH}{sculpture_profit:,d}{C_OFF} credits selling "
-        f"{C_SCULPT}Lunar Sculptures{C_OFF}, consuming {C_EMPH}"
-        f"{sculpture_oxygen_usage}{C_OFF} units of {C_OXY}Oxygen{C_OFF}.")
+    print(f"You made {C.Emph}{sculpture_profit:,d}{C.Off} credits selling "
+        f"{CText.Sculptures}, consuming {C.Emph}"
+        f"{sculpture_oxygen_usage}{C.Off} units of {CText.Oxygen}.")
     
     # Update oxygen used and credits earned    
     dome_state.oxygen -= sculpture_oxygen_usage     
@@ -349,22 +368,22 @@ def show_title():
     pass
 
 def choose_difficulty() -> int:    
-    print(f"{C_EMPH}Lunar Dome - Choose Difficulty Level:{C_OFF}\n\n"
-        f"{C_EMPH}Lunar Dome{C_OFF} offers {C_EMPH}five{C_OFF} levels of "
-        f"progressive difficulty with {C_EASY}1{C_OFF} being the {C_EASY}"
-        f"easiest{C_OFF}\nand {C_HARD}5{C_OFF} the {C_HARD}hardest{C_OFF}.\n\n"
+    print(f"{C.Emph}Lunar Dome - Choose Difficulty Level:{C.Off}\n\n"
+        f"{C.Emph}Lunar Dome{C.Off} offers {C.Emph}five{C.Off} levels of "
+        f"progressive difficulty with {C.Easy}1{C.Off} being the {C.Easy}"
+        f"easiest{C.Off}\nand {C.Hard}5{C.Off} the {C.Hard}hardest{C.Off}.\n\n"
         f"At higher difficulty levels, there is:\n\n"
-        f" * More variation in the cost/value for {C_OXY}Oxygen{C_OFF}, "
-        f"{C_SOUP}Soup{C_OFF} and {C_SCULPT}'Lunar Sculptures'{C_OFF}.\n"
-        f" * A reduction in the number of starting {C_EMPH}Credits{C_OFF}.\n"
+        f" * More variation in the cost/value for {CText.Oxygen}, "
+        f"{CText.Soup} and {CText.Sculptures}.\n"
+        f" * A reduction in the number of starting {CText.Credits}.\n"
         f" * An increase in how fast the population of the colony grows.\n"
-        f" * An increase in the negative impact of {C_BAD}Calamities{C_OFF} "
-        f"vs. {C_GOOD}Boons{C_OFF}.\n"
+        f" * An increase in the negative impact of {C.Bad}Calamities{C.Off} "
+        f"vs. {C.Good}Boons{C.Off}.\n"
         f" * Elimination of low-commodity warnings and estimates.\n"
     )
 
     prompt = (
-        f"Enter Difficulty ({C_EASY}1=Easy{C_OFF}, {C_HARD}5=Hard{C_OFF})")
+        f"Enter Difficulty ({C.Easy}1=Easy{C.Off}, {C.Hard}5=Hard{C.Off})")
     return get_amount(prompt, 1, 5) - 1
 
 def show_instructions():
@@ -383,54 +402,50 @@ def show_instructions():
     if response == "n": return
    
     print(
-        f"{C_EMPH}Lunar Dome - Instructions:{C_OFF}\n\n"
+        f"{C.Emph}Lunar Dome - Instructions:{C.Off}\n\n"
         f"You are the overseer of an new colony living in an experimental "
-        f"'{C_EMPH}Lunar Dome{C_OFF}'.\nYour job is to keep the colony "
+        f"'{C.Emph}Lunar Dome{C.Off}'.\nYour job is to keep the colony "
         f"functioning for as long as possible.\n\n"
-        f"{C_EMPH}Colonists{C_OFF} consume a certain number of units of "        
-        f"{C_OXY}Oxygen{C_OFF} (to breathe) and {C_SOUP}Soup{C_OFF}\n(for "
-        f"food/water) per year. On each turn, you must buy enough "
-        f"{C_OXY}Oxygen{C_OFF} and {C_SOUP}Soup{C_OFF} to\nkeep all the "
-        f"{C_EMPH}Colonists{C_OFF} alive.\n\n"
-        f"The {C_INTEG}Integrity{C_OFF} of the {C_EMPH}Dome{C_OFF} is "
+        f"{CText.Colonists} consume a certain number of units of "        
+        f"{CText.Oxygen} (to breathe) and {CText.Soup}\n(for food/water) per "
+        f"year. On each turn, you must buy enough {CText.Oxygen} and "
+        f"{CText.Soup} to\nkeep all the {CText.Colonists} alive.\n\n"
+        f"The {CText.Integrity} of the {C.Emph}Dome{C.Off} is "
         f"reduced due to wear and tear proportional to the\nnumber of "
-        f"{C_EMPH}Colonists{C_OFF} living in it. A maintenance charge is "
-        f"assessed to restore\nthe {C_EMPH}Dome{C_OFF} to 100% "
-        f"{C_INTEG}Integrity{C_OFF}. If you have insufficient "
-        f"{C_EMPH}Credits{C_OFF} to cover this\ncharge, the "
-        f"{C_INTEG}Integrity{C_OFF} of the {C_EMPH}Dome{C_OFF} will "
+        f"{CText.Colonists} living in it. A maintenance charge is "
+        f"assessed to restore\nthe {C.Emph}Dome{C.Off} to 100% "
+        f"{CText.Integrity}. If you have insufficient {CText.Credits} to cover "
+        f"this\ncharge, the {CText.Integrity} of the {C.Emph}Dome{C.Off} will "
         f"continue to decline.\n\n"
-        f"Beyond your initial budget, additional {C_EMPH}Credits{C_EMPH} can "
-        f"be earned by using excess\n{C_OXY}Oxygen{C_OFF} to make, and sell, "
-        f"'{C_SCULPT}Lunar Sculptures{C_OFF}'. Be careful not to use up ALL\n"
-        f"your {C_OXY}Oxygen{C_OFF} and to keep enough for your "
-        f"{C_EMPH}Colonists{C_OFF}!\n\n"
-        f"{C_EMPH}NOTE:{C_OFF} The price of {C_OXY}Oxygen{C_OFF}, "
-        f"{C_SOUP}Soup{C_OFF}, and the value of '{C_SCULPT}Lunar "
-        f"Sculptures{C_OFF}' will\nfluctuate over time.  Buy low and sell high!"
+        f"Beyond your initial budget, additional {CText.Credits} can "
+        f"be earned by using excess\n{CText.Oxygen} to make, and sell, "
+        f"'{CText.Sculptures}'. Be careful not to use up ALL\n your "
+        f"{CText.Oxygen} and to keep enough for your {CText.Colonists}!\n\n"        
+        f"{C.Emph}NOTE:{C.Off} The price of {CText.Oxygen}, {CText.Soup}, "
+        f"and the value of '{CText.Sculptures}' will\nfluctuate over time. "
+        f"Buy low and sell high!"
         f"\n\n(Press [Enter] to continue ...)"
     )
     input()
     clear_screen()    
     print(
-        f"{C_EMPH}Lunar Dome - Instructions:{C_OFF} (continued ...)\n\n"
-        f"Random events ({C_BAD}'Calamities'{C_OFF}, which are {C_BAD}BAD,"
-        f"{C_OFF} and {C_GOOD}'Boons'{C_OFF}, which are {C_GOOD}GOOD"
-        f"{C_OFF} can\noccur that result in you {C_OFF} or "
-        f"{C_SOUP}Soup{C_OFF}, or result in {C_BAD}damage{C_OFF}\nor "
-        f"{C_GOOD}repairs{C_OFF} to the {C_INTEG}Integrity{C_OFF} of the "
-        f"{C_EMPH}Dome{C_OFF}. If you let your {C_OXY}Oxygen{C_OFF} "
-        f"stores, {C_SOUP}Soup{C_OFF}\nstocks or the {C_EMPH}Dome's{C_OFF} "
-        f"{C_INTEG}Integrity{C_OFF} get too low, these events can be "
-        f"significant\nenough to {C_BAD}end your game!{C_OFF}\n\n"
-        f"If you lack enough {C_OXY}Oxygen{C_OFF} or {C_SOUP}Soup{C_OFF} "
-        f"to sustain all the {C_EMPH}Colonists{C_OFF}, or the\n"
-        f"{C_INTEG}Integrity{C_OFF} of the {C_EMPH}Dome{C_OFF} falls to "
-        f"0, the colony is {C_BAD}no longer viable!{C_OFF} The "
-        f"{C_EMPH}Dome{C_OFF}\nexperiment comes to an end, all the "
-        f"{C_EMPH}Colonists{C_OFF} are returned to Earth, and ... \nthe "
-        f"{C_BAD}GAME is OVER!{C_OFF}\n\n"
-        f"{C_GOOD}GOOD LUCK!{C_OFF}\n\nPress [Enter] when ready.", end=""
+        f"{C.Emph}Lunar Dome - Instructions:{C.Off} (continued ...)\n\n"
+        f"Random events ({C.Bad}'Calamities'{C.Off}, which are {C.Bad}BAD,"
+        f"{C.Off} and {C.Good}'Boons'{C.Off}, which are {C.Good}GOOD"
+        f"{C.Off} can\noccur that result in you {C.Good}gaining{C.Off} or "
+        f"{C.Bad} losing {CText.Oxygen} or {CText.Soup}, or result in {C.Bad}"
+        f"damage{C.Off}\nor {C.Good}repairs{C.Off} to the {CText.Integrity} "
+        f"of the {C.Emph}Dome{C.Off}. If you let your {CText.Oxygen} "
+        f"stores, {CText.Soup}\nstocks or the {C.Emph}Dome's{C.Off} "
+        f"{CText.Integrity} get too low, these events can be "
+        f"significant\nenough to {C.Bad}end your game!{C.Off}\n\n"
+        f"If you lack enough {CText.Oxygen} or {CText.Soup} "
+        f"to sustain all the {CText.Colonists}, or the {CText.Integrity}\nof "
+        f"the {C.Emph}Dome{C.Off} falls to 0, the colony is {C.Bad}no longer "
+        f"viable!{C.Off} The {C.Emph}Dome{C.Off} experiment\ncomes to an end, "
+        f"all the {CText.Colonists} are returned to Earth, and ... \nthe "
+        f"{C.Bad}GAME is OVER!{C.Off}\n\n"
+        f"{C.Good}GOOD LUCK!{C.Off}\n\nPress [Enter] when ready.", end=""
     )
     input()  
     clear_screen()
